b02898942043a98cf14bdb19071b8fc9ea52b95d
aankussshh <108898889+aankusshh@users.noreply.github.com>
2024-04-10 11:33:09 +0530

    Update 1-Create EC2 Instance.tf

--- 1-Create EC2 Instance.tf
+++ 1-Create EC2 Instance.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 # Type "aws config" to mention access_key and secret_key
 
-# You can give the provider by hard code also but its not a good practice for security reasons
+# You can give the provider by hard code also like folowing but its not a good practice for security reasons:
 
 #                    provider "aws" {
 #                         region     = "us-west-2"
@@ -15,7 +15,7 @@ provider "aws" {
 #                         secret_key = "my-secret-key"
 #                    }
 
-# Basic syntax for terraform script
+# Basic syntax for terraform script:
 
 #                   <Resource_type> <Service_type> <Unique_name_you_want_to_provide>{
 #                          <Identifier> = <Expression>
